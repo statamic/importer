7f571d7929a4160e84c3d562163598cceac8c017
Duncan McClean <duncan@duncanmcclean.com>
2024-10-30 13:07:24 +0000

    tweak messaging

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -3,6 +3,6 @@
 return [
     'configuration_instructions' => 'You can add or modify your Blueprint fields to customize what data is imported and what fieldtype it will be stored in. You can save, refresh, and come back to this import config later until it\'s ready to run.',
     'mapping_instructions' => 'Map the fields from your import to the fields in your blueprint.',
-    'migrations_needed' => 'The importer uses Laravel\'s job batching feature to keep track of the import progress, however, it requires a <code>job_batches</code> table in your database. Before you can run the importer, you will need to run <code>php artisan migrate</code>.',
+    'migrations_needed' => 'In order to keep track of import progress, the importer uses Laravel\'s Job Batching feature. It uses a <code>job_batches</code> table in your database to store information about batches. Before you can run the importer, you will need to run the <code>php artisan migrate</code> command.',
     'unique_field_instructions' => 'Select a "unique field" to determine if an item already exists.',
 ];
