8db653e4a14a17d5e64d094b4196f345b2f79ab3
Duncan McClean <duncan@duncanmcclean.com>
2024-11-21 10:35:32 +0000

    Laravel Pint (#40)
    * wip
    
    * Fix styling
    
    * revert
    
    ---------
    
    Co-authored-by: duncanmcclean <duncanmcclean@users.noreply.github.com>

--- tests/Imports/EditImportTest.php
+++ tests/Imports/EditImportTest.php
@@ -15,7 +15,7 @@ class EditImportTest extends TestCase
 {
     use PreventsSavingStacheItemsToDisk;
 
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
 
--- tests/Jobs/ImportItemJobTest.php
+++ tests/Jobs/ImportItemJobTest.php
@@ -17,7 +17,7 @@ class ImportItemJobTest extends TestCase
 {
     use PreventsSavingStacheItemsToDisk;
 
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
 
