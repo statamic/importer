966b3caf40e80d758bb5e0a243d270d4ea1fed95
Max Baan <max@pixelastronauts.nl>
2025-04-07 14:08:11 +0200

    Enhance batch processing in Importer to track current and total batches, ensuring UpdateCollectionTreeJob is dispatched only after the final batch is processed.

--- src/Importer.php
+++ src/Importer.php
@@ -31,16 +31,22 @@ class Importer
         };
 
         $chunks = $items->chunk(500);
+        $pendingBatches = [];
+        $totalBatches = $chunks->count();
+        $currentBatch = 0;
 
         foreach ($chunks as $chunk) {
-            Bus::batch($chunk->map(fn (array $item) => new ImportItemJob($import, $item)))
-                ->before(fn (Batch $batch) => $import->batchId($batch->id)->save())
-            ->finally(function (Batch $batch) use ($import) {
-                if ($import->get('destination.type') === 'entries') {
-                    UpdateCollectionTreeJob::dispatch($import);
-                }
-            })
-            ->dispatch();
+            $currentBatch++;
+            $pendingBatch = Bus::batch($chunk->map(fn(array $item) => new ImportItemJob($import, $item)))
+                ->before(fn(Batch $batch) => $import->batchId($batch->id)->save())
+                ->finally(function (Batch $batch) use ($import, $currentBatch, $totalBatches) {
+                    if ($currentBatch === $totalBatches && $import->get('destination.type') === 'entries') {
+                        UpdateCollectionTreeJob::dispatch($import);
+                    }
+                });
+
+            $pendingBatches[] = $pendingBatch;
+            $pendingBatch->dispatch();
         }
     }
 
